0dc30f6bcef04954945548a65c2cfdcd895fc565
Rafał Chabasiński <r.chabasinski@student.mini.pw.edu.pl>
2020-10-29 01:21:37 +0100

    parser implementation
    Signed-off-by: Rafał Chabasiński <r.chabasinski@student.mini.pw.edu.pl>

---
+++ src/Logic/Services/ParseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PL_Resolution.Logic.Services
+{
+    public class ParseException : Exception
+    {
+        public ParseException(int line, string message)
+        {
+            Message = $"Line {line}: {message}!";
+        }
+
+        public string Message { get; }
+    }
+}
